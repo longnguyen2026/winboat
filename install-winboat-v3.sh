638607c6863468e11bd0952670a6aa9cdbf5a239
nam2026 <cloud07622@gmail.com>
2026-07-30 11:56:53 +0700

    Update install-winboat-v3.sh

--- install-winboat-v3.sh
+++ install-winboat-v3.sh
@@ -18,7 +18,8 @@ CYAN='\033[1;36m'
 RESET='\033[0m'
 
 # WinBoat package
-WB_URL="https://github.com/longnguyen2026/winboat/releases/latest/download/wb.deb"
+#WB_URL="https://github.com/longnguyen2026/winboat/releases/latest/download/wb.deb"
+WB_URL="https://github.com/longnguyen2026/winboat-installer/releases/latest/download/wb.deb"
 
 # ==========================
 # UI
